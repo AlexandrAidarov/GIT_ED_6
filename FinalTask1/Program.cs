fab5ca75be40010e130cf7b840749095e4dee7e5
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-02-16 12:42:19 +0300

    FinalTask1 is done

--- FinalTask1/Program.cs
+++ FinalTask1/Program.cs
@@ -1,10 +1,13 @@
 ﻿
 Console.WriteLine("Введите количество элементов массива:");
 int arrayLength = Convert.ToInt32(Console.ReadLine());
-string[] array = new string[3];
+string[] array = new string[arrayLength];
 
 FillArray(array);
 PrintArray(array);
+
+string[] arrayLengthThree = RebuildArrayLengthThree(array);
+PrintArray(arrayLengthThree);
 
 void FillArray(string[] arr)
 {
@@ -21,8 +24,32 @@ void PrintArray(string[] arr)
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if(i < arr.Length - 1) Console.Write(arr[i]+ ",");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
         else Console.Write(arr[i]);
     }
     Console.WriteLine("]");
+}
+
+string[] RebuildArrayLengthThree(string[] arr)
+{
+    int ArrayLength = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            ArrayLength += 1;
+        }
+    }
+
+    string[] arrThree = new string[ArrayLength];
+
+    for (int i = arr.Length - 1; i > -1; i--)
+    {
+        if (arr[i].Length <= 3)
+        {
+            ArrayLength -= 1;
+            arrThree[ArrayLength] = arr[i];
+        }
+    }
+    return arrThree;
 }
