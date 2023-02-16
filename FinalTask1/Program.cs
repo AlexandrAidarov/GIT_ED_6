606753fb6ebb4a181e9a813a630ae4f5dc3879c6
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-02-16 11:54:47 +0300

    Create/print array add

--- FinalTask1/Program.cs
+++ FinalTask1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите количество элементов массива:");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[3];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива:");
+        string element = Console.ReadLine();
+        arr[i] = element;
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write(arr[i]+ ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
